974e7da70d00c4048eeb3bcd2973f5ff72604fb4
Kostantin Simonenko <kostantin_simonenko@192.168.100.7>
2020-10-04 22:05:12 +0300

    feat: impelment the function reverse()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = Math.abs(n).toString(10);
+    return str.split("").reverse().join("").replace(/$0+/, "");
 }
